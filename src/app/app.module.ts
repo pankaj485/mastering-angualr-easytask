0cfd8cbe7652d0dbfaf2e897a9c6fb67faabcf55
pankaj485 <pankajsamar2@gmail.com>
2024-09-05 18:02:55 +0530

    implement separate module for tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AddTaskComponent } from './tasks/add-task/add-task.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { UserComponent } from './user/user.component';
 import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-    TasksComponent,
-    TaskComponent,
-    AddTaskComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
   bootstrap: [AppComponent],
-  imports: [CommonModule, BrowserModule, FormsModule, SharedModule],
+  imports: [CommonModule, BrowserModule, SharedModule, TasksModule],
 })
 export class AppModule {}
