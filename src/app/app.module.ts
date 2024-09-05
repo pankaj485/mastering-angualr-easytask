4fc70a3bca539f5442ecd7ba0a4e205052c8627d
pankaj485 <pankajsamar2@gmail.com>
2024-09-05 17:52:34 +0530

    add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { CardComponent } from './shared/card/card.component';
 import { AddTaskComponent } from './tasks/add-task/add-task.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CardComponent,
     TasksComponent,
     TaskComponent,
     AddTaskComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [CommonModule, BrowserModule, FormsModule],
+  imports: [CommonModule, BrowserModule, FormsModule, SharedModule],
 })
 export class AppModule {}
